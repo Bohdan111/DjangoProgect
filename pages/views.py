91d7fef2d0fd4917dde83a46511eb0c3379ab37b
Bohdan <mavlyanovbogdan@gmail.com>
2019-02-26 19:17:56 +0200

    views were added

--- pages/views.py
+++ pages/views.py
@@ -4,15 +4,14 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs) :         # *args, **kwargs
-	return HttpResponse("<h1>Home page</h1>")
-	print(request.user)
-
+	#return HttpResponse("<h1>Home page</h1>")
+	return render(request, "home.html",{})
 
 def contact_view(request, *args, **kwargs) :
-	return HttpResponse("<h1>Contact page</h1>")
+	return render(request, "contact.html",{})
 
 def about_view(request,*args, **kwargs) :
-	return HttpResponse("<h1>About page</h1>")
+	return render(request, "about.html",{})
 
 def social_view(request,*args, **kwargs) :
 	return HttpResponse("<h1>Social page</h1>")
--- trydjango/settings.py
+++ trydjango/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'trydjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['/home/bohdan/dev/trydjango/src/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
